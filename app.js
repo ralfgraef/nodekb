cb88e0a5e78d10d02c56e1d5c523052e760b6862
ralfgraef <graef.ralf@gmail.com>
2018-07-29 22:54:50 +0200

    add edit dropdwn and select for authors

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/', function(req, res) {
       console.log(err);
     } else {
       res.render('index', {
-        title: 'Articles',
+        title: 'Einkaufslisten',
         articles: articles
       });
     }
